31563e5b8540076761bbaddd9d6afb553394e040
Arnaud TOURNIER <ltearno@gmail.com>
2022-09-25 09:30:22 +0200

    user friendly

--- cmd/tlsinfo/clienttlsinfo.go
+++ cmd/tlsinfo/clienttlsinfo.go
@@ -8,21 +8,29 @@ import (
 )
 
 func main() {
-	println("hello")
+	port := 1234
+
+	fmt.Printf("Server listens on port %d, it will print the client TLS fingerprint each time it receives a connection\n", port)
+
 	s := &http.Server{
-		Addr:    ":1234",
+		Addr:    fmt.Sprintf(":%d", port),
 		Handler: nilHandler,
 		TLSConfig: &tls.Config{
 			GetConfigForClient: getClientConfig,
 		},
 	}
-	s.ListenAndServeTLS("cert.pem", "key.pem")
+	err := s.ListenAndServeTLS("cert.pem", "key.pem")
+	if err != nil {
+		fmt.Printf("cannot start web server: %s\n", err)
+		fmt.Println("Maybe you forgot to create the TLS certificates ?")
+		fmt.Println("  openssl req -x509 -nodes -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365")
+	}
 }
 
 var nilHandler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(200)
-	w.Write([]byte("{}"))
+	_, _ = w.Write([]byte("{}"))
 }
 
 // called by go runtime when ClientHelloInfo is available
